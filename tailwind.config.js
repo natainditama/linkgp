128ba28ae068474a7e5049ba0a5ac5f6c4a1aa56
natainditama <natainditama@gmail.com>
2022-06-19 05:47:12 +0800

    feat: Setup flowbite and customizing tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {},
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    fontSize: {
+      ...defaultTheme.fontSize,
+      'sm': ['1rem', '1.25rem'],
+      'tiny': ['.875rem', '1.25rem'],
+    },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin'),
+  ] 
 }
